594d67c2b6f6fed01faf91d7099d76de64380258
diegoeduv <diegoeduv@ciencias.unam.mx>
2025-10-08 20:37:42 -0600

    Se agrego main para probar ambas vistas descomentando la vista que se quiera usar

--- app/src/main/java/com/example/tarea_02/MenuActivity.java
+++ app/src/main/java/com/example/tarea_02/MenuActivity.java
@@ -1,6 +1,8 @@
 package com.example.tarea_02;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +10,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
-        //setContentView(R.layout.activity_carrito);;
+        ImageButton btnCarrito = findViewById(R.id.btnCarrito);
+        btnCarrito.setOnClickListener(v -> {
+            Intent i = new Intent(MenuActivity.this, CarritoActivity.class);
+            startActivity(i);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
